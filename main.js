0d82b7112c385369d1a80f104b21c62347bf9077
SahanaBandi <87894811+SahanaBandi@users.noreply.github.com>
2022-04-24 20:28:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+html=loadImage("1200px-HTML_logo_and.svg.png");;
+
+function b2()
+{
+    window.alert("HTML stands for Hyper Text Markup Language. HTML is the standard markup language for creating Web pages. HTML describes the structure of a Web page. HTML consists of a series of elements. HTML elements tell the browser how to display the content.");
+}
